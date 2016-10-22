1db6da52f32469fb78e86f82ea70b11110c81473
Victor G. Brusca <brusca@middlemind.net>
2016-10-22 08:18:22 -0400

    Update the mmg sprite class to use static members for the frame change event it and type.

--- src/net/middlemind/MmgGameApiJava/MmgBase/MmgSprite.java
+++ src/net/middlemind/MmgGameApiJava/MmgBase/MmgSprite.java
@@ -9,6 +9,9 @@ package net.middlemind.MmgGameApiJava.MmgBase;
  */
 public class MmgSprite extends MmgObj {
 
+    public static final int MMG_SPRITE_FRAME_CHANGE_TYPE = 0;
+    public static final int MMG_SPRITE_FRAME_CHANGE = 0;
+    
     /**
      * The origin of the sprite image.
      */
@@ -80,7 +83,7 @@ public class MmgSprite extends MmgObj {
     private boolean simpleRendering;
     private MmgEventHandler onFrameChange;
     private boolean timerOnly;
-    private final MmgEvent frameChange = new MmgEvent(null, "frame_changed", 0, 0, null, null);
+    private final MmgEvent frameChange = new MmgEvent(null, "frame_changed", MmgSprite.MMG_SPRITE_FRAME_CHANGE, MmgSprite.MMG_SPRITE_FRAME_CHANGE_TYPE, null, null);
 
     /**
      * Constructor that sets the MmgBmp array, the source rectangle, the destination rectangle,
