14082d39ad0c7cc891c3e9ba2fe808bdddd55f55
vbrusca <vacuous_b@yahoo.com>
2020-01-23 00:09:29 +0000

    Updated project with the latest gpio example code.

---
+++ src/com/middlemind/OdroidGpio/GamePadExample.java
@@ -0,0 +1,57 @@
+package com.middlemind.OdroidGpio;
+
+import com.middlemind.Odroid.GamePadSimple;
+
+/**
+ *
+ * @author Victor G. Brusca, Middlemind Games
+ * 01/22/2020
+ */
+public class GamePadExample implements GamePadSimple {
+
+    @Override
+    public void ProcessDpadPress(int dir) {
+
+    }
+
+    @Override
+    public void ProcessDpadRelease(int dir) {
+
+    }
+
+    @Override
+    public void ProcessDpadClick(int dir) {
+
+    }
+
+    @Override
+    public void ProcessAPress() {
+        System.out.println("ProcessAPress");
+    }
+
+    @Override
+    public void ProcessARelease() {
+
+    }
+
+    @Override
+    public void ProcessAClick() {
+
+    }
+
+    @Override
+    public void ProcessBPress() {
+
+    }
+
+    @Override
+    public void ProcessBRelease() {
+
+    }
+
+    @Override
+    public void ProcessBClick() {
+
+    }
+
+ }
