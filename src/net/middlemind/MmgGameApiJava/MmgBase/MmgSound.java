9780795102cfb558fd6deaef13a9443ddfb63bef
Victor G. Brusca <brusca@middlemind.net>
2020-06-01 07:21:18 -0400

    Updated MmgAPI with the latest class adjustments from writing.

--- src/net/middlemind/MmgGameApiJava/MmgBase/MmgSound.java
+++ src/net/middlemind/MmgGameApiJava/MmgBase/MmgSound.java
@@ -1,6 +1,7 @@
 package net.middlemind.MmgGameApiJava.MmgBase;
 
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 /**
  * Class that wraps the underlying sound object. 
@@ -36,13 +37,34 @@ public class MmgSound {
     private Clip sound;
 
     /**
+     * The volume set for this sound clip, based on the static class volume.
+     */
+    private float usedVolume;
+    
+    /**
+     * A private class field to track the range of volume for the given sound clip.
+     */
+    private float range;
+    
+    /**
+     * A private class field to track the gain or volume used to set the audio clip's volume.
+     */
+    private float gain;
+    
+    /**
+     * A Java internal class that is used to adjust the volume of the given sound clip.
+     */
+    private FloatControl vol;
+    
+    /**
      * Constructor that sets the sound Clip value.
      *
      * @param se        The sound clip for this sounds object.
      * @see             Clip
      */
     public MmgSound(Clip se) {
-        sound = se;
+        sound = se;        
+        ApplyVolume();
         SetId();
     }
 
@@ -54,9 +76,33 @@ public class MmgSound {
      */
     public MmgSound(MmgSound se) {
         sound = se.GetSound();
+        ApplyVolume();
         SetId();
     }
 
+    /**
+     * Applies the current static volume to this sound clip.
+     */
+    public void ApplyVolume() {
+        usedVolume = MmgSound.volume;        
+        vol = (FloatControl) sound.getControl(FloatControl.Type.MASTER_GAIN);        
+        range = vol.getMaximum() - vol.getMinimum();
+        gain = (range * usedVolume) + vol.getMinimum();
+        vol.setValue(gain);        
+    }
+    
+    /**
+     * Applies the given rate, 0.0 - 1.0, to the sound clip.
+     * 
+     * @param rate  The rate at which that sound clip is played, 0.0 - 1.0, where 1.0 is the maximum rate and 0 is no rate at all.
+     */
+    public void ApplyRate(float rate) {
+        vol = (FloatControl) sound.getControl(FloatControl.Type.SAMPLE_RATE);        
+        float range = vol.getMaximum() - vol.getMinimum();
+        float gain = (range * rate) + vol.getMinimum();
+        vol.setValue(gain);        
+    }    
+    
     /**
      * Sets the volume of the sound system.
      *
@@ -65,7 +111,7 @@ public class MmgSound {
      */
     public static float SetVolume(float f) {
         volume = f;
-        if (volume > 1.3f) {
+        if (volume > 1.0f) {
             volume = 1.0f;
         }
 
@@ -92,7 +138,7 @@ public class MmgSound {
     public int GetId() {
         return id;
     }
-
+    
     /**
      * Sets the unique sound id.
      */
@@ -112,7 +158,7 @@ public class MmgSound {
     }
 
     /**
-     * Gets the low level Clip object.
+     * Gets the lower level Clip object.
      *
      * @return      The sound this class represents.
      * @see         Clip
@@ -122,14 +168,29 @@ public class MmgSound {
     }
 
     /**
+     * Sets the lower level Clip object.
+     * 
+     * @param snd   The sound Clip wrapped by this class.
+     */
+    public void SetSound(Clip snd) {
+        sound = snd;
+        ApplyVolume();
+    }
+    
+    /**
      * Starts playing this sound.
      */
     public void Play() {
         if(sound.isRunning()) {
             sound.stop();
         }
-        
+
+        if(usedVolume != MmgSound.volume) {
+            ApplyVolume();
+        }        
+
         sound.setFramePosition(0);        
+        sound.loop(0);
         sound.start();
     }
 
@@ -144,6 +205,14 @@ public class MmgSound {
             sound.stop();
         }
         
+        if(usedVolume != MmgSound.volume) {
+            ApplyVolume();
+        }
+        
+        if(rate >= 0f) {
+            ApplyRate(rate);
+        }
+        
         sound.setFramePosition(0); 
         sound.loop(loop);
         sound.start();        
@@ -154,5 +223,33 @@ public class MmgSound {
      */
     public void Stop() {
         sound.stop();
+        
+        if(usedVolume != MmgSound.volume) {
+            ApplyVolume();
+        }        
+    }
+    
+    /**
+     * Converts the MmgSound object into a string representation.
+     * 
+     * @return      The string representation of this class.
+     */
+    public String ToString() {
+        return "Sound: " + idStr + " Clip Length MS: " + (sound.getMicrosecondLength() / 1000);
+    }
+    
+    /**
+     * A class method that tests for equality based on the contained sound clip and the sound clip of the
+     * comparison object.
+     * 
+     * @param r     The MmgSound object to compare
+     * @return      A boolean indicating if the object instance is equal to the argument object instance. 
+     */
+    public boolean Equals(MmgSound r) {
+        if (GetSound().equals(r.GetSound()) == true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
