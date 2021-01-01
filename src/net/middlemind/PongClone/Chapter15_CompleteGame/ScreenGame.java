6812a11f491807f3dcbeca6d273f51832e25c6a5
Victor G. Brusca <brusca@middlemind.net>
2021-01-01 00:48:46 -0500

    Updated project with the latest png clone resource files, 8.

--- src/net/middlemind/PongClone/Chapter15_CompleteGame/ScreenGame.java
+++ src/net/middlemind/PongClone/Chapter15_CompleteGame/ScreenGame.java
@@ -891,6 +891,8 @@ public class ScreenGame extends Screen {
         if(bgroundPopupSrc != null) {
             MmgHelper.CenterHorAndVert(bgroundPopupSrc);            
             bgroundPopupSrc = MmgHelper.ContainsKeyMmgBmpScaleAndPosition("popupWindowBase", bgroundPopupSrc, classConfig, bgroundPopupSrc.GetPosition());                        
+            popupTotalWidth = MmgHelper.ScaleValue(300);
+            popupTotalHeight = MmgHelper.ScaleValue(120);             
             bgroundPopup = new Mmg9Slice(16, bgroundPopupSrc, popupTotalWidth, popupTotalHeight);
             bgroundPopup.SetPosition(MmgVector2.GetOriginVec());
             MmgHelper.CenterHorAndVert(bgroundPopup);
